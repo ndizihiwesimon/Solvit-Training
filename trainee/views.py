4301d85938167d6b8072c5f09ab519d9935f27cf
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-22 09:39:54 +0200

    Trainee form errors

--- trainee/views.py
+++ trainee/views.py
@@ -7,15 +7,18 @@ from trainee.models import Trainee
 
 def trainees_list(request):
     trainees = Trainee.objects.all()
-    context = {
-        "trainees": trainees,
-    }
+    
     
     if request.method == 'POST':
         form = TraineeForm()
         trainee_form = form(request.POST)
         if trainee_form.is_valid():
             trainee_form.save()
+        else:
+            context = {
+        "trainees": trainees,
+        "form": trainee_form.errors,
+    }
     else:
         return render(request, 'trainees/list.html', context)
 
