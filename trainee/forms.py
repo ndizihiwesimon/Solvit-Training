78589405cb7929e2054bb2d68bbe42d7792f4f0e
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-22 09:44:33 +0200

    validate_full_name

--- trainee/forms.py
+++ trainee/forms.py
@@ -5,5 +5,12 @@ class TraineeForm(forms.Form):
     full_name = forms.CharField(required=True)
     dob = forms.DateField(required=True, widget=forms.DateInput())
     
-    def save(self):
-        pass
+    def validate_full_name(self, value):
+        try:
+            Trainee.objects.get(full_name=value)
+            raise forms.ValidationError("This name is already in use")
+        except Trainee.DoesNotExist:
+            return value
+    
+    def save(self, validated_data):
+        tr = Trainee(full_name=validated_data['full_name'], dob=validated_data['dob'])
