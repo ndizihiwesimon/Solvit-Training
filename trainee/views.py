9f7bb8c0ba39bf7fd76f486bfbaffe2c080eb0a7
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-22 10:08:20 +0200

    Working on forms

--- trainee/views.py
+++ trainee/views.py
@@ -8,7 +8,6 @@ from trainee.models import Trainee
 def trainees_list(request):
     trainees = Trainee.objects.all()
     
-    
     if request.method == 'POST':
         form = TraineeForm()
         trainee_form = form(request.POST)
@@ -16,10 +15,22 @@ def trainees_list(request):
             trainee_form.save()
         else:
             context = {
-        "trainees": trainees,
-        "form": trainee_form.errors,
-    }
-    else:
-        return render(request, 'trainees/list.html', context)
+                "trainees": trainees,
+                "form": trainee_form.errors,
+            }
+            return render(request, 'trainees/list.html', context)
 
     
+# Listing
+# adding -- using forms, modelForms, or forms
+
+# Editing -- View and template only
+# Deleting -- View and template only
+# Last one -- Login Template
+
+# create 5 views
+# 1) listing all trainees
+# 2) adding a trainee (Forms)
+# 3) editing a trainee  NB on the view and the template
+# 4) deleting a trainee NB on the view and the template
+# 5) login template
