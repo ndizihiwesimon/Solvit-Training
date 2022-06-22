1d64bfe39664c70131c5fa74ac1ad585986a26fa
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-22 08:21:13 +0200

    first commit

---
+++ trainee/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Trainee(models.Model):
+    full_name = models.CharField(max_length=100, blank=False)
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.full_name
