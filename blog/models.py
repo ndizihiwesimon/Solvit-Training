dab65419475607673a4f1ec97ffd931f0720ee2b
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-24 16:21:17 +0200

    Created comment model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.timezone import now
 # Create your models here.
 
 STATUS = (
@@ -23,3 +23,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    blog = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
+    dateTime = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.user.username + " Comment: " + self.content
