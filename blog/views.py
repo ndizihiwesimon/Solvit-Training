e223d7989e91b3317aff35e0dce9cc9ad6fa1324
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-28 07:52:52 +0200

    Working on views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Post
+from .forms import BlogForm
 
 # Create your views here.
 
+
+def blog(request):
+    blogs = Post.objects.all()
+    context = {"blogs": blogs}
+    return render(request, 'blog.html', context)
+
+
+def add_blog(request):
+    form = BlogForm()
+    context = {"form": form}
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST)
+        if blog_form.is_valid():
+            blog_form.save()
+            return redirect("blogs")
+        else:
+            context = {'form': blog_form}
+            return render(request, 'add_form.html', context)
+    return redirect(request, 'add_form.html', context)
 
 
 # class PostList(generic.ListView):
