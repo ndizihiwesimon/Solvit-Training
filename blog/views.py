cc2d78c308d2f2727f3b61179cc161896c29e7db
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-28 07:54:39 +0200

    Commented ones from Shema

--- blog/views.py
+++ blog/views.py
@@ -6,24 +6,24 @@ from .forms import BlogForm
 # Create your views here.
 
 
-def blog(request):
-    blogs = Post.objects.all()
-    context = {"blogs": blogs}
-    return render(request, 'blog.html', context)
+# def blog(request):
+#     blogs = Post.objects.all()
+#     context = {"blogs": blogs}
+#     return render(request, 'blog.html', context)
 
 
-def add_blog(request):
-    form = BlogForm()
-    context = {"form": form}
-    if request.method == 'POST':
-        blog_form = BlogForm(request.POST)
-        if blog_form.is_valid():
-            blog_form.save()
-            return redirect("blogs")
-        else:
-            context = {'form': blog_form}
-            return render(request, 'add_form.html', context)
-    return redirect(request, 'add_form.html', context)
+# def add_blog(request):
+#     form = BlogForm()
+#     context = {"form": form}
+#     if request.method == 'POST':
+#         blog_form = BlogForm(request.POST)
+#         if blog_form.is_valid():
+#             blog_form.save()
+#             return redirect("blogs")
+#         else:
+#             context = {'form': blog_form}
+#             return render(request, 'add_form.html', context)
+#     return redirect(request, 'add_form.html', context)
 
 
 # class PostList(generic.ListView):
