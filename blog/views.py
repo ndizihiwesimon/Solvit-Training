c4c0bfbe6f7b3c2a0f1662277a332adf61810cb2
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-06-28 07:42:35 +0200

    Pushed down a bit

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 
 # Create your views here.
 
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/index.html'
